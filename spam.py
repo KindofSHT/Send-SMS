9db64be8cdec348051a93e2a1ae966bb3fc2d7d9
Hanz ain`t enough <90843849+KindofSHT@users.noreply.github.com>
2023-05-04 03:13:23 +0800

    Update spam.py
    🤕

--- spam.py
+++ spam.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = 'Your Account SID'
-auth_token = 'Your Auth Token'
+account_sid = 'AC8ea09d3669f4ece1e6e680e76dce24df'
+auth_token = '6a1e107325c25e7c7dbbb3fffd77b573'
 client = Client(account_sid, auth_token)
 
 for i in range(100):
     message = client.messages.create(
-        body='Hello from Twilio!',
-        from_='Your Twilio Number',
-        to='Your Phone Number'
+        body='HAHAHAHHAHAHAHAHAHAHHAHAHAHAHAHHHAHAHAHHAHAHA!',
+        from_='+13203149868',
+        to='09917712508'
     )
 
     print(f"SMS number {i+1} sent with SID: {message.sid}")
